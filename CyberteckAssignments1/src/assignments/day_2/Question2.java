7a8ab4a9a85e255ef4c2315c82d87b0af4014432
AzizTuprak <a.abdirshit@gmail.com>
2021-06-25 16:29:17 +0200

    question 2

---
+++ CyberteckAssignments1/src/assignments/day_2/Question2.java
@@ -0,0 +1,12 @@
+package assignments.day_2;
+
+public class Question2 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		String firstName="Alan";
+		String lastName="Turing";
+System.out.println(firstName+","+lastName);
+	}
+
+}
